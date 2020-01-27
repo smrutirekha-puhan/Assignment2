59ba4cdc8f58c551b441bd62906bd2160d36afea
smrutirekha-puhan <59962350+smrutirekha-puhan@users.noreply.github.com>
2020-01-27 20:14:14 +0530

    Add files via upload

---
+++ EmployeeSalary/EmployeeSalary/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeSalary
+{
+    public abstract class Employee
+    {
+        public int EmpId;
+        public string EmpName;
+        public string Designation;
+        public int Exp;
+        public double AnnualSal;
+        public DateTime Date;
+
+        public abstract double CalculateSalary();
+
+    }
+
+
+    public class HR : Employee
+        {
+            private double basicsalary;
+        
+
+        public override double CalculateSalary()
+        {
+            double basicsalary = 8000;
+            int Exp = 4;
+            return basicsalary * 1000 * (Exp);
+        }
+
+        
+    }
+
+        public class Developer : Employee
+        {
+            private double basicsalary1;
+
+        public override double CalculateSalary()
+            {
+            double basicsalary1 = 8000;
+            int Exp = 5;
+            return basicsalary1 * 2000 * Exp;
+            }
+        }
+
+
+
+    
+
+}
---
+++ EmployeeSalary/EmployeeSalary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EmployeeSalary.Employee;
+
+namespace EmployeeSalary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp = new HR();
+            Employee emp2 = new Developer();
+
+            Console.WriteLine("HR Salary :" + emp.CalculateSalary());
+            Console.WriteLine("Developer Salary :" + emp2.CalculateSalary());
+
+            Console.ReadKey();
+
+        }
+    }
+}
